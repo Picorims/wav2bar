4be5a5fe7153258ac626c16d006e2a8d6dfbd7f0
Picorims <picorims.contact@gmail.com>
2023-02-08 19:55:32 +0100

    feat(state machine): support array indexes in paths

--- js/utils/state_machine_mixin.js
+++ js/utils/state_machine_mixin.js
@@ -79,7 +79,7 @@ export let StateMachineMixin = {
             let val = object[key];
             let path = (root === "") ? key : `${root}/${key}`;
             keys.push(path);
-            if (type.IsAnObject(val) && !type.IsAnArray(val)) {
+            if (type.IsAnObject(val)) {//object or array? deep explore
                 keys = [...keys, ...this._getStatePaths(val, path)];
             }
         }
@@ -140,6 +140,7 @@ export let StateMachineMixin = {
 
         //change the value of the desired key
         let is_obj = type.IsAnObject(value) && !type.IsAnArray(value);
+        let is_array = type.IsAnArray(value);
         if (is_obj) {
             //if it is an object, change all of its listed states.
             let object = value;
@@ -148,6 +149,13 @@ export let StateMachineMixin = {
                     let modifiedLocal = this.setState(`${state_path}/${key}`, object[key], true);
                     if (modifiedLocal) modified = true;
                 }
+            }
+        } else if (is_array) {
+            //if it is an array, change all of its index values.
+            let array = value;
+            for (let i = 0; i < array.length; i++) {
+                let modifiedLocal = this.setState(`${state_path}/${i}`, array[i], true);
+                if (modifiedLocal) modified = true;
             }
         } else {
             //assign
--- test/renderer/utils.test.js
+++ test/renderer/utils.test.js
@@ -702,6 +702,9 @@ describe('utils', () => {
                                 expect(class_instance.getState(path)).to.deep.equal(value);
                             } else if (value instanceof Array) {
                                 expect(class_instance.getState(path)).to.deep.equal(value);
+                                for (let i=0; i < value.length; i++) {
+                                    expect(class_instance.getState(path+"/"+i)).to.deep.equal(value[i]);
+                                }
                             } else {
                                 expect(class_instance.getState(path)).to.equal(value);
                             }
@@ -730,6 +733,9 @@ describe('utils', () => {
                     c.setState("d", {f: "test2"});
                     expect(c.getState("d")).to.deep.equal({e: true, f: "test2"});
                     expect(c.getState("d/f")).to.equal("test2");
+
+                    c.setState("c/0", 3);
+                    expect(c.getState("c/0")).to.equal(3);
                 });
 
                 it('does not allow NaN', () => {
@@ -745,6 +751,7 @@ describe('utils', () => {
                     expect(() => {c.getState("d/unknown")}).to.throw();
                     expect(() => {c.setState("d/unknown", 2)}).to.throw();
                     expect(() => {c.setState("d", {unknown: "a"})}).to.throw();
+                    expect(() => {c.setState("c/2", 5)}).to.throw();
                 });
     
                 it('supports using validators on states', () => {
@@ -767,7 +774,7 @@ describe('utils', () => {
                     let c = new testClass2();
     
                     this.timeout(1000);
-                    expect(c._state_paths).to.be.of.length(6);
+                    expect(c._state_paths).to.be.of.length(8);
     
                     c.subscribeToState("a", (value) => {
                         expect(value).to.equal("foo");
@@ -776,7 +783,7 @@ describe('utils', () => {
                     c.setState("a", "foo");
                 });
 
-                it('allows to subscribe to a state change happening in children', function(done) {
+                it('allows to subscribe to a state change happening in children (object)', function(done) {
                     let c = new testClass2();
     
                     this.timeout(1000);
@@ -786,6 +793,42 @@ describe('utils', () => {
                         done();
                     })
                     c.setState("d/f", 5);
+                });
+    
+                it('allows to subscribe to a state change happening in children (array)', function(done) {
+                    let c = new testClass2();
+    
+                    this.timeout(1000);
+    
+                    c.subscribeToState("c", (value) => {
+                        expect(value[0]).to.deep.equal(15);
+                        done();
+                    })
+                    c.setState("c/0", 15);
+                });
+    
+                it('allows to subscribe to a state change happening in parent (object)', function(done) {
+                    let c = new testClass2();
+    
+                    this.timeout(1000);
+    
+                    c.subscribeToState("d/f", (value) => {
+                        expect(value).to.deep.equal(5);
+                        done();
+                    })
+                    c.setState("d", {e: true, f: 5});
+                });
+    
+                it('allows to subscribe to a state change happening in parent (array)', function(done) {
+                    let c = new testClass2();
+    
+                    this.timeout(1000);
+    
+                    c.subscribeToState("c", (value) => {
+                        expect(value).to.deep.equal([15,5]);
+                        done();
+                    })
+                    c.setState("c/0", 15);
                 });
     
                 it('lets unsubscribe to state', () => {
@@ -823,7 +866,7 @@ describe('utils', () => {
                             path: "c",
                             other_machine: c3,
                             other_path: "tab",
-                            test_val: [45,89,87],
+                            test_val: [45,87],
                         },
                         {
                             machine: c3,
