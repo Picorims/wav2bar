ed1e99d89372c3564130c864470adf54ed770dd9
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2025-02-23 12:48:25 +0100

    chore(app): comment devtools

--- main.js
+++ main.js
@@ -151,7 +151,7 @@ function createWindow () {
     win.loadFile("index.html");
 
     // Open the DevTools.
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
 
     //Hide menu bar
     win.setMenuBarVisibility(false);
